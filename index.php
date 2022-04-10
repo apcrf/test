9fc769a5b38bf21686967fa707ef6c176aaac6e3
Alexander I <apcrf@mail.ru>
2022-04-10 20:01:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<title>monte-new</title>
+	<link rel="icon" type="image/x-icon" href="favicon.ico"/>
+</head>
+<body>
+</body>
+</html>
